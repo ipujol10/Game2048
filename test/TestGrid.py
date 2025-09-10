939e91996ab6766bad02081b1f47aad6528eb156
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2025-09-10 12:58:15 +0200

    test grid movements single number

--- test/TestGrid.py
+++ test/TestGrid.py
@@ -1,5 +1,41 @@
+"""Testing the Grid class"""
+
 import unittest
+from src.Grid import Grid
 
 
-class TestGrid(unittest.TestCase):
-    pass
+class TestGridMove(unittest.TestCase):
+    """Tests for the move method in the Grid class"""
+
+    def testMoveSingle(self) -> None:
+        """Test the move method with a single number"""
+        top_right = [[0 for _ in range(4)] for _ in range(4)]
+        top_right[0][3] = 2
+        top_left = [[0 for _ in range(4)] for _ in range(4)]
+        top_left[0][0] = 2
+        bottom_left = [[0 for _ in range(4)] for _ in range(4)]
+        bottom_left[3][0] = 2
+        bottom_right = [[0 for _ in range(4)] for _ in range(4)]
+        bottom_right[3][3] = 2
+        top_center = [[0 for _ in range(4)] for _ in range(4)]
+        top_center[0][2] = 2
+
+        grid: Grid = Grid()
+        grid.grid[2][2] = 2
+
+        grid.up()
+        self.assertEqual(grid.grid, top_center)
+        grid.right()
+        self.assertEqual(grid.grid, top_right)
+        grid.down()
+        self.assertEqual(grid.grid, bottom_right)
+        grid.left()
+        self.assertEqual(grid.grid, bottom_left)
+        grid.up()
+        self.assertEqual(grid.grid, top_left)
+        grid.right()
+        self.assertEqual(grid.grid, top_right)
+
+
+if __name__ == "__main__":
+    unittest.main()
