ff4ac39c745b33c20fdbfb587dee1053274362c0
ipujol10 <31001088+ipujol10@users.noreply.github.com>
2025-09-26 13:21:35 +0200

    Test movement with separated numbers

--- test/TestGrid.py
+++ test/TestGrid.py
@@ -1,5 +1,6 @@
 """Testing the Grid class"""
 
+import copy
 from itertools import repeat
 import unittest
 from src.Grid import Grid
@@ -8,17 +9,20 @@ from src.Grid import Grid
 class TestGridMove(unittest.TestCase):
     """Tests for the move method in the Grid class"""
 
+    def _generateGrid(self) -> list[list[int]]:
+        return [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+
     def testMoveSingle(self) -> None:
         """Test the move method with a single number"""
-        top_right = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        top_right = self._generateGrid()
         top_right[0][3] = 2
-        top_left = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        top_left = self._generateGrid()
         top_left[0][0] = 2
-        bottom_left = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        bottom_left = self._generateGrid()
         bottom_left[3][0] = 2
-        bottom_right = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        bottom_right = self._generateGrid()
         bottom_right[3][3] = 2
-        top_center = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        top_center = self._generateGrid()
         top_center[0][2] = 2
 
         grid: Grid = Grid()
@@ -51,16 +55,16 @@ class TestGridMove(unittest.TestCase):
 
     def testMultiplenNumbersPack(self) -> None:
         """Test the move method when there are multiple numbers involved in a pack"""
-        top_right = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        top_right = self._generateGrid()
         top_right[0][2:] = [2, 4]
         top_right[1][2:] = [4, 2]
-        top_left = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        top_left = self._generateGrid()
         top_left[0][:2] = [2, 4]
         top_left[1][:2] = [4, 2]
-        bottom_right = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        bottom_right = self._generateGrid()
         bottom_right[2][2:] = [2, 4]
         bottom_right[3][2:] = [4, 2]
-        bottom_left = [[0 for _ in repeat(None, 4)] for _ in repeat(None, 4)]
+        bottom_left = self._generateGrid()
         bottom_left[2][:2] = [2, 4]
         bottom_left[3][:2] = [4, 2]
 
@@ -75,12 +79,52 @@ class TestGridMove(unittest.TestCase):
         grid.right()
         self.assertEqual(grid.grid, top_right)
 
-    @unittest.skip("Need to implement")
     def testMultipleNumbersSeparated(self) -> None:
         """Test the move method when there are multiple numbers involded separated"""
+        start = self._generateGrid()
+        start[0][0] = 2
+        start[2][0] = 4
+        end = self._generateGrid()
+        end[0][0] = 2
+        end[1][0] = 4
+
+        grid: Grid = Grid()
+        grid.grid = copy.deepcopy(start)
+        grid.up()
+        self.assertEqual(grid.grid, end)
+
+        end = self._generateGrid()
+        end[2][0] = 2
+        end[3][0] = 4
+        grid.grid = copy.copy(start)
+        grid.down()
+        self.assertEqual(grid.grid, end)
+
+        start = self._generateGrid()
+        start[0][0] = 2
+        start[0][2] = 4
+        end = self._generateGrid()
+        end[0][2] = 2
+        end[0][3] = 4
+
+        grid.grid = copy.deepcopy(start)
+        grid.right()
+        self.assertEqual(grid.grid, end)
+
+        end = self._generateGrid()
+        end[0][0] = 2
+        end[0][1] = 4
+
+        grid.grid = copy.deepcopy(start)
+        grid.left()
+        self.assertEqual(grid.grid, end)
 
     @unittest.skip("Need to implement")
-    def testMultipleMergins(self) -> None:
+    def testSimpleMergings(self) -> None:
+        """Test when there are just 2 numbers able to merge"""
+
+    @unittest.skip("Need to implement")
+    def testMultipleMergings(self) -> None:
         """Test when threre are several possible merges"""
 
 
